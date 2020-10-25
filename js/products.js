09312732825a90de07be7105c32c4e995fcdc2e7
rijfas <rijfas01@gmail.com>
2020-10-25 21:20:11 +0530

    first-bug-fix

--- js/products.js
+++ js/products.js
@@ -25,7 +25,7 @@ function SetProductsView(data){
         categorieName.innerText = element['categorieName'];
         row.appendChild(categorieName);
         element['items'].forEach(item =>{
-            if(i<=3){
+            if(i<=1){
                 document.getElementById('autocomplete-input').placeholder += item['name'] + ',';
                 i++;
             }
@@ -38,7 +38,7 @@ function SetProductsView(data){
             var card = document.createElement('div');
             card.classList.add('card');
             card.classList.add('z-depth-3');
-            card.innerHTML =  '<div class="card-image"><img src="products/img/'+ item['image']+'" alt=""></div><div class="card-content"><span class="card-title bold">'+ item['name'] +'</span>'+ item['description'] +'</div><div class="card-action"><h4 class="red-text">AED '+ item['price'] +'</h4><a href="#" class="btn blue">Order Now</a>';
+            card.innerHTML =  '<div class="card-image"><img src="products/img/'+ item['image']+'" alt=""></div><div class="card-content"><span class="card-title bold">'+ item['name'] +'</span>'+ item['description'] +'</div><div class="card-action"><h4 class="red-text">AED '+ item['price'] +'</h4><a target="_blank" href="https://wa.me/971558525262?text=Hi!%20I%20Would%20Like%20To%20Purchase%20'+item['name'].replace(' ','%20')+'" class="btn blue">Order Now</a>';
             outerColumn.appendChild(card);
             row.appendChild(outerColumn);
         });
