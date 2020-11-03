9c61c780729e524f621095398f69ed42f2ccd55a
rijfas <rijfas01@gmail.com>
2020-11-03 19:03:23 +0530

    mobile search fix

--- js/search.js
+++ js/search.js
@@ -1,6 +1,8 @@
 function searchProduct(item){
   try{
-    document.getElementById(item.replace(' ','_')).scrollIntoView();
+    var offset = document.getElementsByClassName('navbar-fixed')[0].style.height;
+    var searchElement = document.getElementById(item.replace(' ','_')); 
+    window.scroll({ top: (searchElement.offsetTop - offset), left: 0, behavior: 'smooth' });
   }
   catch{
 
