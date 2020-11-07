e4d7ec973499fe754f1c3ecf055ff50c942954c4
rijfas <rijfas01@gmail.com>
2020-11-07 21:14:45 +0530

    products updated

--- js/products.js
+++ js/products.js
@@ -23,6 +23,8 @@ function SetProductsView(data){
         var i = 1;
         var row = document.createElement("div");
         row.classList.add('row');
+        var innerRow = document.createElement("div");
+        innerRow.classList.add('flex');
         var categorieName = document.createElement("h4");
         categorieName.className = 'center';
         categorieName.innerText = element['categorieName'];
@@ -37,13 +39,14 @@ function SetProductsView(data){
             outerColumn.id = setNameToId(item['name']);
             outerColumn.classList.add('col');
             outerColumn.classList.add('s12');
-            outerColumn.classList.add('m4')
+            outerColumn.classList.add('m4');
             var card = document.createElement('div');
             card.classList.add('card');
             card.classList.add('z-depth-3');
             card.innerHTML =  '<div class="card-image"><img src="products/img/'+ item['image']+'" alt=""></div><div class="card-content"><span class="card-title bold">'+ item['name'] +'</span>'+ item['description'] +'</div><div class="card-action"><h4 class="red-text">AED '+ item['price'] +'</h4><a target="_blank" href="https://wa.me/971558525262?text=Hi!%20I%20Would%20Like%20To%20Purchase%20'+item['name'].replace(' ','%20')+'" class="btn blue">Order Now</a>';
             outerColumn.appendChild(card);
-            row.appendChild(outerColumn);
+            innerRow.appendChild(outerColumn);
+            row.appendChild(innerRow);
         });
         productsView.appendChild(row);
         var currentPlaceholder = document.getElementById('autocomplete-input').placeholder;
