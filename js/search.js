163c155a61763cb09bac25d4ef787eaa50625f77
rijfas <rijfas01@gmail.com>
2020-11-07 21:45:46 +0530

    search highlight

--- js/search.js
+++ js/search.js
@@ -3,6 +3,10 @@ function searchProduct(item){
     var offset = document.getElementsByClassName('navbar-fixed')[0].style.height;
     var searchElement = document.getElementById(item.replace(' ','_')); 
     window.scroll({ top: (searchElement.offsetTop - offset), left: 0, behavior: 'smooth' });
+    searchElement.style.backgroundColor = '#2962FF';
+    window.setTimeout(function(){
+      searchElement.style.backgroundColor = 'transparent';
+    },600);
   }
   catch{
 
